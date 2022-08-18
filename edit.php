e7ef14c4aca73ce11183add58bbd58c8dd883bb2
ArtcalO <niyca14@gmail.com>
2022-08-18 12:22:31 +0200

    edit llivre working

--- edit.php
+++ edit.php
@@ -3,16 +3,17 @@
 include_once("config.php");
 
 // Check if form is submitted for user update, then redirect to homepage after update
-if(isset($_POST['update']))
+if(isset($_POST['modifier']))
 {	
 	$id = $_POST['id'];
 	
-	$name=$_POST['name'];
-	$mobile=$_POST['mobile'];
-	$email=$_POST['email'];
+	$nom=$_POST['nom'];
+	$categorie=$_POST['categorie'];
+	$date_sortie=$_POST['date_sortie'];
+	$auteur=$_POST['auteur'];
 		
 	// update user data
-	$result = mysqli_query($mysqli, "UPDATE users SET name='$name',email='$email',mobile='$mobile' WHERE id=$id");
+	$result = mysqli_query($mysqli, "UPDATE livre SET nom='$nom',categorie='$categorie',date_sortie='$date_sortie',auteur='$auteur' WHERE id=$id");
 	
 	// Redirect to homepage to display updated user in list
 	header("Location: index.php");
@@ -24,41 +25,46 @@ if(isset($_POST['update']))
 $id = $_GET['id'];
 
 // Fetech user data based on id
-$result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
+$result = mysqli_query($mysqli, "SELECT * FROM livre WHERE id=$id");
 
 while($user_data = mysqli_fetch_array($result))
 {
-	$name = $user_data['name'];
-	$email = $user_data['email'];
-	$mobile = $user_data['mobile'];
+	$nom = $user_data['nom'];
+	$categorie = $user_data['categorie'];
+	$date_sortie = $user_data['date_sortie'];
+	$auteur = $user_data['auteur'];
 }
 ?>
 <html>
 <head>	
-	<title>Edit User Data</title>
+	<title>Modification du livre</title>
 </head>
 
 <body>
-	<a href="index.php">Home</a>
+	<a href="index.php">Accueil</a>
 	<br/><br/>
 	
 	<form name="update_user" method="post" action="edit.php">
 		<table border="0">
 			<tr> 
-				<td>Name</td>
-				<td><input type="text" name="name" value=<?php echo $name;?>></td>
+				<td>Nom</td>
+				<td><input type="text" name="name" value=<?php echo $nom;?>></td>
 			</tr>
 			<tr> 
-				<td>Email</td>
-				<td><input type="text" name="email" value=<?php echo $email;?>></td>
+				<td>Categorie</td>
+				<td><input type="text" name="email" value=<?php echo $categorie;?>></td>
 			</tr>
 			<tr> 
-				<td>Mobile</td>
-				<td><input type="text" name="mobile" value=<?php echo $mobile;?>></td>
+				<td>Date_sortie</td>
+				<td><input type="text" name="mobile" value=<?php echo $date_sortie;?>></td>
+			</tr>
+			<tr> 
+				<td>Auteur</td>
+				<td><input type="text" name="mobile" value=<?php echo $auteur;?>></td>
 			</tr>
 			<tr>
 				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
-				<td><input type="submit" name="update" value="Update"></td>
+				<td><input type="submit" name="modifier" value="Modifier"></td>
 			</tr>
 		</table>
 	</form>
